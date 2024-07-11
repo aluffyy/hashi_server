208a321000d63d2de2afccf5d78d886e07a65f69
Mustakim Islam Alif <alifisalu@gmail.com>
2024-07-11 10:44:35 +0600

    docker setup

--- routes/user/index.dart
+++ routes/user/index.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
+
+import '../../prisma/generated_dart_client/client.dart';
+import '../../prisma/generated_dart_client/prisma.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   return switch (context.request.method) {
@@ -23,15 +27,26 @@ Future<Response> _getUsers() {
 
 Future<Response> _createUser(RequestContext context) async {
   final json = (await context.request.json()) as Map<String, dynamic>;
-  final name = json['name'];
-  final lastname = json['lastname'];
+  final name = json['name'] as String?;
+  final lastname = json['lastname'] as String?;
+
+  if (name == null || lastname == null) {
+    return Response.json(body: {
+      'message': 'Name and Lastname are required',
+    }, statusCode: HttpStatus.badRequest);
+  }
+
+  final prisma = PrismaClient(
+    datasources: Datasources(
+        db: "mysql://root:123456@localhost:3306/mydb?schema=public"),
+  );
+  final user = await prisma.user.create(
+    data: UserCreateInput(name: name, lastname: lastname),
+  );
   return Response.json(
     body: {
       'message': 'Saved!',
-      'user': {
-        'name': name,
-        'lastname': lastname,
-      },
+      'user': user,
     },
   );
 }
